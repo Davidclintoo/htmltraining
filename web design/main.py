8658c991ebf94d5343740071ce913503566945c1
david onyango <clintoodavi01@gmail.com>
2022-03-26 11:30:16 +0300

    latest

--- web design/main.py
+++ web design/main.py
@@ -2,9 +2,10 @@
 from cProfile import label
 from enum import unique
 from unicodedata import name
-from flask import Flask, flash ,render_template, request ,redirect, url_for ,session 
+from flask import Flask, flash ,render_template, request ,redirect, url_for ,session, flash
 
 import psycopg2
+import psycopg2.extras
 import os
 import secrets
 import re
@@ -18,13 +19,11 @@ conn=psycopg2.connect("dbname='duka' user='postgres' host='localhost' password='
 
 
 
-@app.route('/index')
+@app.route("/")
 def home():
     return  render_template("index.html") 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+
 
 @app.route("/achievements")
 def achievement():
@@ -32,44 +31,43 @@ def achievement():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")      
+    return render_template("about.html") 
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")     
 
 @app.route("/signup" ,methods=["GET", "POST"])
 def sinup():
     msg=''
     if request.method=="POST" and 'first_name' in request.form and 'second_name' in request.form and 'password' in request.form and 'email' in request.form:
-        cur=conn.cursor()
+        cur=conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
         first_name=request.form["first_name"]
         second_name=request.form["second_name"],
         email=request.form["email"],
         password=request.form["password"]
+        cur.execute("SELECT  * from users  where first_name =first_name , second_name=second_name , email=email , password=password  ;" )
+        account = cur.fetchone()
+
         
-        if "@" in email and "." in email :
-         msg="welcome"
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', first_name):
+            msg = 'Username must contain only characters and numbers !'
+        elif not first_name or  not second_name or not password or not email:
+            msg = 'Please fill out the form !'
         else:
-          msg="invalid email try again "
-
-
-
-
-
-        users=[] 
-        for x in users:
-            if x.first_name==first_name and x.second_name==second_name and x.email==email and x.password==password:
-                return redirect(url_for('login')) 
-
-            else:
+            cur.execute("INSERT INTO public.users(id, first_name, second_name, email, password)VALUES (null, %s, %s, %s, %s);")
+            conn.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('contact.html', msg = msg)
         
        
-        
-      
-                     query='INSERT INTO public.users(  first_name, second_name, email, password) VALUES (%s, %s, %s, %s);'
-                     rows=(first_name,second_name ,email,password)
-                     cur.execute(query ,rows)
-                     conn.commit()
-
-    return redirect(url_for("login" ,msg=msg)) 
 
 
 
